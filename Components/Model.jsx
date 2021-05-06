eb1750486cc055e2a2efa8fc141a38bca2c61f29
E-boi <hiboysismyoufake6789@gmail.com>
2021-05-05 20:47:11 -0400

    tiny changes like make icons take you to github

--- Components/Model.jsx
+++ Components/Model.jsx
@@ -4,6 +4,9 @@ const { get } = require('powercord/http');
 const { React } = require('powercord/webpack');
 const { Modal } = require('powercord/components/modal');
 const { decrypt } = require('../crypto');
+const {
+	shell: { openExternal },
+} = require('electron');
 
 module.exports = class githubModel extends React.PureComponent {
 	constructor() {
@@ -12,17 +15,22 @@ module.exports = class githubModel extends React.PureComponent {
 	}
 
 	componentDidMount() {
+		const state = {};
 		const repo = get(`https://api.github.com/repos/${this.props.link[3]}/${this.props.link[4]}/contents`);
 		if (this.props.getSetting('api-key')) repo.set('Authorization', `token ${decrypt(this.props.getSetting('api-key'))}`);
-		repo.then(res => this.setState({ data: res }));
+		repo.then(res => (state.data = res.body));
 
 		const branches = get(`https://api.github.com/repos/${this.props.link[3]}/${this.props.link[4]}/branches`);
 		if (this.props.getSetting('api-key')) branches.set('Authorization', `token ${decrypt(this.props.getSetting('api-key'))}`);
-		branches.then(res => this.setState({ branches: res.body }));
+		branches.then(res => (state.branches = res.body));
 
 		const defaultB = get(`https://api.github.com/repos/${this.props.link[3]}/${this.props.link[4]}`);
 		if (this.props.getSetting('api-key')) defaultB.set('Authorization', `token ${decrypt(this.props.getSetting('api-key'))}`);
-		defaultB.then(res => this.setState({ rootInfo: res.body, selectedBranch: res.body.default_branch }));
+		defaultB.then(res => {
+			state.repoInfo = res.body;
+			state.selectedBranch = res.body.default_branch;
+			this.setState(state); // only 1 rerender
+		});
 	}
 
 	changeBranch(branch) {
@@ -32,10 +40,13 @@ module.exports = class githubModel extends React.PureComponent {
 	}
 
 	render() {
+		console.log(this.state.repoInfo);
 		return (
 			<Modal className="githubModel">
 				<Modal.Header>
-					<p>{this.props.link[4]}</p>
+					<p className="repo-name" onClick={() => openExternal(this.state.repoInfo?.html_url)}>
+						{this.props.link[4]}
+					</p>
 					{this.state.branches && (
 						<select className="Gbranches" value={this.state.selectedBranch} onChange={change => this.changeBranch(change.currentTarget.value)}>
 							{this.state.branches.map(branch => (
@@ -43,29 +54,37 @@ module.exports = class githubModel extends React.PureComponent {
 							))}
 						</select>
 					)}
-					{this.state.rootInfo && <div className="star-svg">
-                                <svg height={16} viewBox="0 0 16 16">
-                                    <path
-                                        fill-rule="evenodd"
-                                        d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
-                                    ></path>
-                                </svg>
-                                <p>{this.state.rootInfo.stargazers_count}</p>
-                            </div>}
+					{this.state.repoInfo && (
+						<div className="star-svg" onClick={() => openExternal(`${this.state.repoInfo.html_url}/stargazers`)}>
+							<svg height={16} viewBox="0 0 16 16">
+								<path
+									fill-rule="evenodd"
+									d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
+								></path>
+							</svg>
+							<p>{this.state.repoInfo.stargazers_count}</p>
+						</div>
+					)}
 				</Modal.Header>
 				<Modal.Content>
 					{this.state.data && (
 						<div>
-							{this.state.data.body.map(tree => (
-								<p className={tree.type === 'dir' ? 'Gfolder' : 'Gfile'}>
+							{this.state.data.map(tree => (
+								<p
+									className={[
+										tree.type === 'dir' ? 'Gfolder' : 'Gfile',
+										tree.type !== 'dir' ? tree.name.split('.')[tree.name.split('.').length - 1] : '',
+										tree.type !== 'dir' ? (tree.name.includes('.') ? '' : 'blank') : '',
+									]
+										.join(' ')
+										.trimEnd()}
+								>
 									{tree.type === 'dir' ? (
 										<img src="https://raw.githubusercontent.com/Pavui/Assets/main/svg-path.svg" height={16} width={16} />
 									) : (
 										<img src="https://raw.githubusercontent.com/Pavui/Assets/main/svg-path%20(1).svg" height={16} width={16} />
 									)}
-									<a onClick={() => require('electron').shell.openExternal(tree.html_url)}>
-										{tree.name} {tree.type === 'dir' && '(FOLDER)'}
-									</a>
+									<a onClick={() => openExternal(tree.html_url)}>{tree.name}</a>
 								</p>
 							))}
 						</div>
@@ -80,11 +99,20 @@ module.exports = class githubModel extends React.PureComponent {
 					>
 						Close
 					</Button>
-					{this.state.rootInfo && (
+					{this.state.repoInfo && (
 						<div className="repo-info">
-							<div className="owner-profile">
-								<img height={32} width={32} src={this.state.rootInfo.owner.avatar_url} />
-								<p>{this.state.rootInfo.owner.login}</p>
+							<div className="owner-profile" onClick={() => openExternal(this.state.repoInfo.owner.html_url)}>
+								<img height={32} width={32} src={this.state.repoInfo.owner.avatar_url} />
+								<p>{this.state.repoInfo.owner.login}</p>
+							</div>
+							<div className="fork-svg" onClick={() => openExternal(`${this.state.repoInfo.html_url}/network/members`)}>
+								<svg viewBox=" 0 0 16 16" height={16}>
+									<path
+										fill-rule="evenodd"
+										d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"
+									></path>
+								</svg>
+								<p>{this.state.repoInfo.forks}</p>
 							</div>
 						</div>
 					)}
