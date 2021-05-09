72b11306b6af38e3bf010e60fda63c074d45c9c7
E-boi <hiboysismyoufake6789@gmail.com>
2021-05-09 14:17:05 -0400

    update icon source

--- Components/Model.jsx
+++ Components/Model.jsx
@@ -11,6 +11,10 @@ const {
 } = require('electron');
 
 const imageTypes = ['png', 'jpg'];
+const folderIcon = 'https://raw.githubusercontent.com/Loremly/Assets/main/github%20icons/folder.svg';
+const fileIcon = 'https://raw.githubusercontent.com/Loremly/Assets/main/github%20icons/file.svg';
+const starIcon = 'https://raw.githubusercontent.com/Loremly/Assets/main/github%20icons/repo-stars.svg';
+const forkIcon = 'https://raw.githubusercontent.com/Loremly/Assets/main/github%20icons/repo-forks.svg';
 
 module.exports = class githubModel extends React.PureComponent {
 	constructor() {
@@ -95,12 +99,7 @@ module.exports = class githubModel extends React.PureComponent {
 					</p>
 					{this.state.repoInfo && (
 						<div className="star-svg" onClick={() => openExternal(`${this.state.repoInfo.html_url}/stargazers`)}>
-							<svg height={16} viewBox="0 0 16 16">
-								<path
-									fill-rule="evenodd"
-									d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
-								></path>
-							</svg>
+							<img src={starIcon} />
 							<p>{this.state.repoInfo.stargazers_count}</p>
 						</div>
 					)}
@@ -141,7 +140,7 @@ module.exports = class githubModel extends React.PureComponent {
 								<p>{`/${path}/`}</p>
 							</div>
 							<div className="Gback-button">
-								<img src="https://raw.githubusercontent.com/Pavui/Assets/main/svg-path.svg" height={16} width={16} />
+								<img src={folderIcon} height={16} width={16} />
 								<a onClick={() => this.goBack()}>Back</a>
 							</div>
 							{this.state.folder.map(tree => (
@@ -155,18 +154,12 @@ module.exports = class githubModel extends React.PureComponent {
 										.trimEnd()}
 								>
 									{tree.type === 'dir'
-										? [
-												<img src="https://raw.githubusercontent.com/Pavui/Assets/main/svg-path.svg" height={16} width={16} />,
-												<a onClick={() => this.viewFolder(tree.path)}>{tree.name}</a>,
-										  ]
-										: [
-												<img src="https://raw.githubusercontent.com/Pavui/Assets/main/svg-path%20(1).svg" height={16} width={16} />,
-												<a onClick={() => this.openFile(tree.name)}>{tree.name}</a>,
-										  ]}
+										? [<img src={folderIcon} height={16} width={16} />, <a onClick={() => this.viewFolder(tree.path)}>{tree.name}</a>]
+										: [<img src={fileIcon} height={16} width={16} />, <a onClick={() => this.openFile(tree.name)}>{tree.name}</a>]}
 								</p>
 							))}
 						</div>
-					)}{' '}
+					)}
 					{!this.state.folder && !this.state.file && this.state.rootDir && (
 						<div className="Gout-folder">
 							{this.state.rootDir?.map(tree => (
@@ -180,14 +173,8 @@ module.exports = class githubModel extends React.PureComponent {
 										.trimEnd()}
 								>
 									{tree.type === 'dir'
-										? [
-												<img src="https://raw.githubusercontent.com/Pavui/Assets/main/svg-path.svg" height={16} width={16} />,
-												<a onClick={() => this.viewFolder(tree.path)}>{tree.name}</a>,
-										  ]
-										: [
-												<img src="https://raw.githubusercontent.com/Pavui/Assets/main/svg-path%20(1).svg" height={16} width={16} />,
-												<a onClick={() => this.openFile(tree.name)}>{tree.name}</a>,
-										  ]}
+										? [<img src={folderIcon} height={16} width={16} />, <a onClick={() => this.viewFolder(tree.path)}>{tree.name}</a>]
+										: [<img src={fileIcon} height={16} width={16} />, <a onClick={() => this.openFile(tree.name)}>{tree.name}</a>]}
 								</p>
 							))}
 						</div>
@@ -209,12 +196,7 @@ module.exports = class githubModel extends React.PureComponent {
 								<p>{this.state.repoInfo.owner.login}</p>
 							</div>
 							<div className="fork-svg" onClick={() => openExternal(`${this.state.repoInfo.html_url}/network/members`)}>
-								<svg viewBox=" 0 0 16 16" height={16}>
-									<path
-										fill-rule="evenodd"
-										d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"
-									></path>
-								</svg>
+								<img src={forkIcon} />
 								<p>{this.state.repoInfo.forks}</p>
 							</div>
 						</div>
