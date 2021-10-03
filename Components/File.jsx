fd133b57c30cf5bf6fa650920ad86ab051d51305
e-boi <hiboysismyoufake6789@gmail.com>
2021-10-03 13:43:41 -0400

    better

--- Components/File.jsx
+++ Components/File.jsx
@@ -1,16 +1,20 @@
 const { React, getModule } = require('powercord/webpack');
 const parser = getModule(['parse', 'parseTopic'], false);
+const classes = {
+	markup: getModule(['markup'], false).markup,
+	scrollbarGhostHairline: getModule(['scrollbarGhostHairline'], false).scrollbarGhostHairline,
+};
 
 module.exports = class githubModel extends React.PureComponent {
 	render() {
 		const { file, path } = this.props;
 		return (
-			<div>
+			<div className={classes.markup}>
 				<div className='Gpath'>
 					<p>{`/${path}`}</p>
 				</div>
 				{file.isImage && (
-					<div className='Gimg scrollbarGhostHairline-1mSOM1'>
+					<div className={`Gimg ${classes.scrollbarGhostHairline}`}>
 						<img src={`data:${file.type};base64,${file.content}`} />
 					</div>
 				)}
